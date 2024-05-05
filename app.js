2a350e70ef5b282110b68b9a9874603b071f6593
Pavan Kalyan Konudula <pkonudul@uncc.edu>
2024-05-05 12:35:42 -0400

    speech endpoint added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var readline = require("readline");
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
 const fs = require("fs");
 const express = require('express');
@@ -100,6 +101,78 @@ app.post('/transcribe', async (req, res) => {
         res.status(500).send('Error retrieving audio file');
     }
 });
+
+
+//endpoint which takes input text and converts it to speech
+/**
+ * @swagger
+ * /speech:
+ *   post:
+ *     summary: Convert text to speech
+ *     description: This endpoint accepts text in the request body and generates a corresponding audio file.
+ *     parameters:
+ *       - name: text
+ *         in: query
+ *         type: string
+ *         description: Text to convert
+ *         required: true
+ *     produces:
+ *       - audio/wav
+ *     responses:
+ *       200:
+ *         description: Successful conversion
+ *         schema:
+ *           type: file
+ *           example: YourAudioFile.wav
+ *       499:
+ *         description: Conversion canceled or error occurred
+ *         schema:
+ *           type: string
+ *           example: "CANCELED: Reason=SpeechTimeout"
+ *       500:
+ *         description: Error converting text to speech
+ *         schema:
+ *           type: string
+ *           example: "Error converting text to speech"
+ */
+app.post('/speech', (req, res) => {
+    const text = req.query.text;
+    var audioFile = "YourAudioFile.wav";
+    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+
+    // The language of the voice that speaks.
+    speechConfig.speechSynthesisVoiceName = "en-US-AvaMultilingualNeural"; 
+
+    // Create the speech synthesizer.
+    var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    
+
+    synthesizer.speakTextAsync(text,
+        function (result) {
+      if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+        // console.log("synthesis finished.");
+        res.download('YourAudioFile.wav');
+      } else {
+        // console.error("Speech synthesis canceled, " + result.errorDetails +
+        //     "\nDid you set the speech resource key and region values?");
+        res.send("Speech synthesis canceled, " + result.errorDetails).status(499);
+      }
+      synthesizer.close();
+      synthesizer = null;
+    },
+        function (err) {
+      console.trace("err - " + err);
+      synthesizer.close();
+      synthesizer = null;
+    });
+})
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
